cc195eb08ab764b815988caf8d0e88d31b30d34d
shain <shain1912@gmail.com>
2026-06-04 19:35:45 +0900

    chore(hub): default 실행.bat to port 47801 (avoid extension conflict)

--- hub/실행.bat
+++ hub/실행.bat
@@ -3,7 +3,8 @@ setlocal
 title MTB Hub
 cd /d "%~dp0"
 if "%MTB_PASSWORD%"=="" set "MTB_PASSWORD=changeme1234"
-echo Starting MTB Hub on http://127.0.0.1:47800
+if "%MTB_PORT%"=="" set "MTB_PORT=47801"
+echo Starting MTB Hub on http://127.0.0.1:%MTB_PORT%
 echo Password: %MTB_PASSWORD%
 call npx tsx src/index.ts
 pause
